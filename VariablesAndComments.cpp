7e30c54088592f460066600a365a09a0118d1828
Ronti0005 <rontisingh24012007@gmail.com>
2025-05-05 12:48:49 +0530

    Adding the first variable and comments to the repo

---
+++ VariablesAndComments.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std ;
+int main(){
+    cout<<"I am Ronit Kumar Singh";
+    return 0;
+}
+
+// Hii Buddy , light weight .
+/*
+Hii 
+I am 
+Ronit Kumar Singh
+*/
